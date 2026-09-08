e6879e6e56bfadbc597393c6b36464ecfdbe7f48
Nickop21 <ny8244@gmail.com>
2026-09-08 12:12:16 +0530

    handling request with url package

--- server.js
+++ server.js
@@ -4,14 +4,22 @@ const url=require("url")
 const myserver=http.createServer((req,res)=>{
     console.log("server started");
     const log=`${Date.now()}:${req.url} New req Received\n`
+    const my_url=url.parse(req.url,true)
+        console.log(my_url);
+        
     fs.appendFile("log.txt",log,(err,data)=>{
-        switch(req.url){
+        switch(my_url.pathname){
             case "/":
-                res.end("i m at homapage")
+             if (req.method=="GET") res.end("i m at homapage")
                 break;
             case "/user":
-                res.end("i m at user page")
+                const userid=my_url.query.userid
+                const usename=my_url.query.name
+                res.end(`i m at user page of ${usename} and id is ${userid} `)
                 break;
+            case "/signup":
+                if (req.method=="GET") res.end("This is a signup form")
+                 else if(req.method=="POST") res.end("Sucess")   
             default:
                 res.end("404 Error")        
         }
